43c39b708f9e81a48d71446fafa65c49a4ac465f
tiago <tiagohngl@gmail.com>
2015-12-19 20:50:34 -0200

    Added some dotfiles and more programs in the post install script

--- arch/post-install.sh
+++ arch/post-install.sh
@@ -13,58 +13,102 @@ fi
 
 check_user
 
+# {{
 
-print_title "BASIC TOOLS"
-pacman -S --noconfirm wget git emacs
+  print_title "BASIC TOOLS"
+  pacman -S --noconfirm wget git curl bc rsync mlocate bash-completion vim tar gzip bzip2 unzip unrar p7zip dbus ntfs-3g ntfsprogs dosfstools openssh samba alsa-lib alsa-utils alsa-plugins xterm fortune cowsay
 
-git config --global user.name "Tiago Engel"
-git config --global user.email "tiagohngl@gmail.com"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=3600'
+  systemctl enable dbus
+  systemctl enable sshd
 
-sumary "Basic tools installation"
-finish_function
+  cp /etc/samba/smb.conf.default /etc/samba/smb.conf
+
+  # Battery improvement
+  su -l $USERNAME --command="yaourt -S --noconfirm tlp"
+  systemctl enable tlp
+
+  sumary "Basic tools installation"
+  finish_function
+
+# }}
+
+# {{
+
+  print_title "YAOURT - https://wiki.archlinux.org/index.php/Yaourt"
+  pacman -S --noconfirm base-devel yajl
+
+  su -l $USERNAME --command="
+  wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz;
+  tar zxvf package-query.tar.gz;
+  cd package-query;
+  makepkg --noconfirm;
+  "
+  cd /home/$USERNAME/package-query
+  install_package
+  cd ..
+  rm -fr package-query*
+
+  su -l $USERNAME --command="
+  wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz;
+  tar zxvf yaourt.tar.gz;
+  cd yaourt;
+  makepkg --noconfirm;
+  "
+  cd /home/$USERNAME/yaourt
+  install_package
+  cd ..
+  rm -fr yaourt*
+
+  sumary "Yaourt installation"
+  finish_function
+
+# }}
+
+# {{
+
+  print_title "ZSH"
+  su -l $USERNAME --command="yaourt -S --noconfirm zsh oh-my-zsh-git powerline-fonts-git"
+  sumary "Zsh installation"
+  finish_function
+
+# }}
 
 
-print_title "YAOURT - https://wiki.archlinux.org/index.php/Yaourt"
-pacman -S --noconfirm base-devel yajl
+# {{
 
-su -l $USERNAME --command="
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz;
-tar zxvf package-query.tar.gz;
-cd package-query;
-makepkg --noconfirm;
-"
-cd /home/$USERNAME/package-query
-install_package
-cd ..
-rm -fr package-query*
+  print_title "DEVELOPMENT TOOLS"
+  pacman -S --noconfirm jdk7-openjdk jdk8-openjdk maven apache-ant scala sbt nodejs npm meld emacs ruby jruby rbenv
+  su -l $USERNAME --command="yaourt -S --noconfirm leiningen leiningen-completions sublime-text-dev" 
 
-su -l $USERNAME --command="
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz;
-tar zxvf yaourt.tar.gz;
-cd yaourt;
-makepkg --noconfirm;
-"
-cd /home/$USERNAME/yaourt
-install_package
-cd ..
-rm -fr yaourt*
+  archlinux-java set jdk7-openjdk
 
-sumary "Yaourt installation"
-finish_function
+  sumary "Development Tools"
+  finish_function
+
+# }}
 
 
-print_title "ZSH"
-su -l $USERNAME --command="yaourt -S --noconfirm zsh oh-my-zsh-git powerline-fonts-git"
-sumary "Zsh installation"
-finish_function
+# {{
+
+  print_title "FONTS"
+  pacman -S --noconfirm ttf-dejavu ttf-liberation
+  su -l $USERNAME --command="yaourt -S --noconfirm ttf-ms-fonts"
+  sumary "Fonts installation"
+  finish_function 
+
+# }}
 
 
-print_title "DEVELOPMENT TOOLS"
-pacman -S --noconfirm jdk7-openjdk jdk8-openjdk maven apache-ant scala sbt nodejs npm leiningen leiningen-completions 
-sumary "Development Tools"
-finish_function
+# {{
+
+  print_title "DOCKER"
+  pacman -S --noconfirm docker
+  groupadd docker
+  gpasswd -a $USERNAME docker
+  sumary "Docker installation"
+
+# }}
+
 
 # print_title "INTEL GPU DRIVER"
 # pacman -S --noconfirm xf86-video-intel
@@ -86,23 +130,38 @@ finish_function
 # install_status
 # sumary "BUMBLEBEE installation"
 
-print_title "XORG SERVER"
-pacman -S --noconfirm xorg-server xorg-server-utils
-sumary "Xorg server installation"
+# {{
+
+  print_title "XORG SERVER"
+  pacman -S --noconfirm xorg-server xorg-server-utils xf86-video-vesa xorg-xinit xorg-xkill
+  sumary "Xorg server installation"
+
+# }}
 
 
-# print_title "KDE"
-# pacman -S --noconfirm plasma sddm
-# systemctl enable sddm
-# # Do not upgrade kde wallpapers 
-# echo "
-# IgnorePkg=plasma-workspace-wallpapers
-# IgnorePkg=kde-wallpapers
-# IgnorePkg=kdeartwork-wallpapers
-# IgnorePkg=kdeartwork-weatherwallpapers
-# " >> /etc/pacman.conf
-# sumary "Kde installation"
+# {{
+
+  print_title "KDE"
+  pacman -S --noconfirm plasma sddm yakuake dolphin kate ark phonon-qt5 phonon-qt5-gstreamer phonon-qt4 phonon-qt4-gstreamer kmix
+  systemctl enable sddm
+  # Do not upgrade kde wallpapers 
+  # echo "
+  # IgnorePkg=plasma-workspace-wallpapers
+  # IgnorePkg=kde-wallpapers
+  # IgnorePkg=kdeartwork-wallpapers
+  # IgnorePkg=kdeartwork-weatherwallpapers
+  # " >> /etc/pacman.conf
+  sumary "Kde installation"
+
+# }}
 
 
+# Dotfiles
+cd ..
+chmod +x bootstrap.sh
+su $USERNAME --command="./bootstrap.sh"
 
-
+git config --global user.name "Tiago Engel"
+git config --global user.email "tiagohngl@gmail.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
